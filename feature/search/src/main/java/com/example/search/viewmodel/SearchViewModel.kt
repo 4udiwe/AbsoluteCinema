9faaa5e166575377fe1fbeb33602b5849c970e52
4udiwe <arsenijsarifkulov2633@gmail.com>
2025-04-24 14:33:08 +0300

    SearchState & ViewModel refactoring

--- feature/search/src/main/java/com/example/search/viewmodel/SearchViewModel.kt
+++ feature/search/src/main/java/com/example/search/viewmodel/SearchViewModel.kt
@@ -4,7 +4,6 @@ import android.content.SharedPreferences
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domain.model.Movie
 import com.example.domain.repository.MovieRepository
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -14,65 +13,65 @@ import kotlinx.coroutines.launch
 
 class SearchViewModel(
     private val repository: MovieRepository,
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
 ) : ViewModel() {
-    private val _movies = MutableStateFlow<List<Movie>>(emptyList())
-    val movies = _movies.asStateFlow()
-    private val _errorState = MutableStateFlow(false)
-    val errorState = _errorState.asStateFlow()
-    private val _emptyState = MutableStateFlow(false)
-    val emptyState = _emptyState.asStateFlow()
-    private val _isSearching = MutableStateFlow(false)
-    val isSearching = _isSearching.asStateFlow()
+
+    private val _searchState = MutableStateFlow<SearchState>(SearchState.Idle)
+    val searchstate = _searchState.asStateFlow()
+
     private val _searchHistory = MutableStateFlow<List<String>>(emptyList())
     val searchHistory = _searchHistory.asStateFlow()
+
     // Ключ для хранения истории в SharedPreferences
     companion object {
         private const val SEARCH_HISTORY_KEY = "search_history"
     }
+
     // Загрузка истории поиска из SharedPreferences
     init {
         loadSearchHistory()
     }
+
     // Загрузка истории поиска
     private fun loadSearchHistory() {
-        val history = sharedPreferences.getStringSet(SEARCH_HISTORY_KEY, setOf())?.toList() ?: emptyList()
+        val history =
+            sharedPreferences.getStringSet(SEARCH_HISTORY_KEY, setOf())?.toList() ?: emptyList()
         _searchHistory.value = history
     }
+
     // Сохранение истории поиска
     private fun saveSearchHistory() {
         val historySet = _searchHistory.value.toSet()
         sharedPreferences.edit().putStringSet(SEARCH_HISTORY_KEY, historySet).apply()
     }
+
     // Добавление нового запроса в историю
     private fun addToSearchHistory(query: String) {
         val updatedHistory = (_searchHistory.value + query).takeLast(10)
         _searchHistory.value = updatedHistory
         saveSearchHistory()
     }
+
     // Очистка истории поиска
     fun clearSearchHistory() {
         _searchHistory.value = emptyList()
         sharedPreferences.edit().remove(SEARCH_HISTORY_KEY).apply()
     }
+
     private suspend fun search(query: String) {
-        _isSearching.value = true
-        _errorState.value = false
-        _emptyState.value = false
+        _searchState.value = SearchState.Loading
         try {
-            _movies.value = repository.searchMoviesByName(query).movies
-            Log.d("Search", "query: $query")
+            val result = repository.searchMoviesByName(query)
+            if (result.total == 0)
+                _searchState.value = SearchState.Empty
+            else
+                _searchState.value = SearchState.Success(movies = result.movies)
         } catch (e: Exception) {
+            _searchState.value = SearchState.Error(message = e.message ?: "Unknown error")
             Log.e("Search", e.message.toString())
-            _errorState.value = true
-            _movies.value = emptyList()
-        } finally {
-            if (!errorState.value && movies.value.isEmpty()) {
-                _emptyState.value = true
-            }
-            _isSearching.value = false
         }
     }
+
     private var searchJob: Job? = null
     fun searchWithDebounce(query: String, launchedFromButton: Boolean) = viewModelScope.launch {
         searchJob?.cancel()
@@ -86,11 +85,9 @@ class SearchViewModel(
             addToSearchHistory(query = query)
         }
     }
-    fun clearState(){
-        _movies.value = emptyList()
-        _errorState.value = false
-        _emptyState.value = false
-        _isSearching.value = false
+
+    fun clearState() {
+        _searchState.value = SearchState.Idle
         searchJob?.cancel()
     }
 }
